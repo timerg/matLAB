d50ae5122dd2413ed55c27853c166518b45a2364
timer <ggteven@gmail.com>
2015-04-29 21:34:54 +0800

    slightly good
    35.8% 784x60 etaj=etak=0.01

--- hw4/Hw4_784weightspluslayer.m
+++ hw4/Hw4_784weightspluslayer.m
@@ -6,40 +6,44 @@ clear all; close all;
 Ntrain = 699; %800 pictures seperate into two parts
 Ntest = 99;
 W = 28; %28*28 picso
-wi = ones(W^2,1);
-wj = zeros(W^2,20).*0.2;
-wk = ones(20,10).*0.5;
+wj = roundn(rand(W^2,50),-1);
+wk = roundn(rand(50,10),-1);
 % wj_all = zeros(W,10)
 Amean = ones(W^2,10);
-eta = 0.01;
+etak = 0.01;
+etaj = 0.01
 %% read images and calculate the mean image for each digit
-for ll = 0:1000
+for ll = 0:Ntrain*60
     for k = 0;
-        for n = 0:9;
+%         for n = 1:1;
+            
+        n = ceil(rand*10)-1
+        c = ceil(rand*0.7*1000)-1
             d = zeros(1,10);
-            d(n+1) = 1
+            d(n+1) = 0.5
 
-            for c = [0:(k*100-1),(k*100+100):(Ntrain+100)];
+%             for c = [0:(k*100-1),(k*100+100):(Ntrain+100)];
                 fname = sprintf('/Users/timer/OneDrive/ms1_2/neuralnetwork/hw4/data/digit_%1d_%03d.bmp',n,c);
                 B = double(imread(fname));
                 xj = reshape(B./255,784,1);
 
-                yj = xj'*wj;%wj':1x20
+                yj = xj'*wj./784;%wj':1x20
                 xk = 1./(1.+exp(-yj));  %1x20
-                yk = xk*wk;% 1x10;
-                err = d-yk;
+                yk = xk*wk./20;% 1x10;
+                err = d-yk
 
                 %%% change all
+                
                 for nn = 0:9;
-
+                delta = err(nn+1)*wk(:,nn+1).*xk'.*(1.-xk');
+                wj = wj + etaj.*xj*delta';
 
                 %%% change once a time
-                delta = err(nn+1)*wk(:,nn+1).*xk'.*(1.-xk');
-                wj = wj + eta.*xj*delta';
-                wk(:,nn+1) = wk(:,nn+1) + eta*err(:,nn+1).*xk';
+
+                wk(:,nn+1) = wk(:,nn+1) + etak*err(:,nn+1).*xk';
                 end
-            end
-        end
+%             end
+%         end
     end
 end
         for n = 0:9; 
@@ -51,14 +55,16 @@ end
 %% testing
 accuracy = zeros(1,10);
 confusion = zeros(10,10);
+Fin_val = zeros(100,10)
 for CL = 1:10
     for c = 700:799
         dist = zeros(1,10);
-        fname = sprintf('digit_%1d_%03d.bmp', CL-1, c);
-        A = double(imread(['/Users/timer/OneDrive/ms1_2/neuralnetwork/hw4/data/' fname]))./255;
+        fnamet = sprintf('digit_%1d_%03d.bmp', CL-1, c);
+        A = double(imread(['/Users/timer/OneDrive/ms1_2/neuralnetwork/hw4/data/' fnamet]))./255;
         dist = (reshape(A,784,1)'*wj*wk);
         [y,indmin] = max(dist);
         confusion(CL,indmin) = confusion(CL,indmin)+1;
+        Fin_val(c-699,CL) = y;
     end
 end
 fprintf('Total Accuracy = %2.1f%%\n',100*sum(diag(confusion))/sum(sum(confusion)));
