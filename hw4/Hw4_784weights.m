119bff00fe103aaf9823b01516c753f0b7edcba3
timer <ggteven@gmail.com>
2015-04-30 00:54:06 +0800

    add random to easier
    also change not only correspond digit weights but also other weights
    every time

--- hw4/Hw4_784weights.m
+++ hw4/Hw4_784weights.m
@@ -12,8 +12,10 @@ wj = ones(W^2,10).*0.5;
 Amean = ones(W^2,10);
 eta = 0.0001;
 %% read images and calculate the mean image for each digit
-for k = 0:0;
-for n = 0:9;
+for k = 0:7840;
+% for n = 0:9;
+    n = ceil(rand*10)-1;
+    c = ceil(rand*0.7*1000)-1;
     d = zeros(10,1);
 %      for c1 = Ntrain:799;
 %         fname1 = sprintf('/Users/timer/OneDrive/ms1_2/neuralnetwork/hw4/data/digit_%1d_%03d.bmp',n,c1);
@@ -23,9 +25,9 @@ for n = 0:9;
 %         
 %      end
 %         dforb = sum(d./(28*100));
-    d(n+1) = 1;
+    d(n+1) = 1
     
-    for c = [0:(k*100-1),(k*100+100):(Ntrain+100)];
+%     for c = [0:(k*100-1),(k*100+100):(Ntrain+100)];
         fname = sprintf('/Users/timer/OneDrive/ms1_2/neuralnetwork/hw4/data/digit_%1d_%03d.bmp',n,c);
         B = double(imread(fname));
         xi = reshape(B./255,784,1);
@@ -45,16 +47,17 @@ for n = 0:9;
 %         errn = 0.5-yj(n2)';
 %         wj(:,n2) = wj(:,n2) + eta*errn.*xj;
 
-    end
+end
+    for n = 0:9;
       Amean(:,n+1) = reshape(wj(:,n+1).*255*100,W^2,1);
     figure(1); subplot(3,4,n+1); 
     imshow(reshape(Amean(:,n+1),W,W),[0 255])  
-    
+    end
     
 
-end
+% end
     
-end
+% end
 
 
 %% testing
