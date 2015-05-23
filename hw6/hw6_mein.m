f0b5c73d5d35aea6b8189079c31b88af539d58af
timer <ggteven@gmail.com>
2015-05-23 16:01:25 +0800

    gap(1:cc)=>gap(1,cc)

--- hw6/hw6_mein.m
+++ hw6/hw6_mein.m
@@ -6,21 +6,19 @@ rvalue = (rand(784,50)-0.5).*40;
 wij = roundn(rvalue,-0) ;
 
 for c = 0:0;
-fname = sprintf('~/OneDrive/ms1_2/neuralnetwork/hw6/2_train/digit_2_%03d.bmp',c);
-fname = sprintf('~Users/timer/OneDrive/ms1_2/neuralnetwork/hw6/2_train/digit_2_%03d.bmp',c);
+% fname = sprintf('~/OneDrive/ms1_2/neuralnetwork/hw6/2_train/digit_2_%03d.bmp',c);
+fname = sprintf('/Users/timer/OneDrive/ms1_2/neuralnetwork/hw6/2_train/digit_2_%03d.bmp',c);   %for windows
 A = double(imread(fname));
 vi = reshape(A./255,784,1);
 
 En  = (vi)'*wij.*(hj)';      %En is 1x50
 En_ = (vi)'*wij.*(abs(hj-1))';
 gap = En - En_
-  for cc = 1:50
-    if gap(1:cc) > 0
-      hj(cc,:) = hj(cc,:);
-      0
-    elseif gap(1:cc) < 0
-      hj(cc,:) = abs(hj(cc,:)-1);
-      1
+  for cc = 1:50;
+    if gap(1,cc) > 0;
+      hj(cc,1) = hj(cc,1);
+  elseif gap(1,cc) < 0;
+      hj(cc,1) = abs(hj(cc,1)-1);
     end
   end
 
